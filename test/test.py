0dbfce3094d78fa9cf6db73d3ccab2a1efd44060
brow1950 <brow1950@purdue.edu>
2023-04-02 13:15:07 -0400

    initial testing environment

---
+++ test/test.py
@@ -0,0 +1,23 @@
+import re2
+
+def main():
+    assert(concatenate())
+    assert(repetition())
+    assert(disjunction())
+    assert(grouping())
+    return(1)
+
+def concatenate():
+    return(1)
+
+def repetition():
+    return(1)
+
+def disjunction():
+    return(1)
+
+def grouping():
+    return(1)
+
+if __name__ == "__main__":
+    main()
